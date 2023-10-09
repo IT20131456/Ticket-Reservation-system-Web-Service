826da6987411aa8ed90dcb1ac7f72b047ccfacdb
IT20128036 <it20128036@my.sliit.lk>
2023-10-10 03:03:54 +0530

    updated

--- Services/TravelerService.cs
+++ Services/TravelerService.cs
@@ -8,37 +8,63 @@ namespace MongoDotnetDemo.Services
     {
         private readonly IMongoCollection<Traveler> _travelerCollection;
         private readonly IOptions<DatabaseSettings> _dbSettings;
+        private readonly ILogger<TravelerService> _logger;
 
-        public TravelerService(IOptions<DatabaseSettings> dbSettings)
+
+
+        public TravelerService(IOptions<DatabaseSettings> dbSettings, ILogger<TravelerService> logger)
         {
             _dbSettings = dbSettings;
-            var mongoClient= new MongoClient(dbSettings.Value.ConnectionString);
-            var mongoDatabase= mongoClient.GetDatabase(dbSettings.Value.DatabaseName);
-            _travelerCollection = mongoDatabase.GetCollection<Traveler>
-                (dbSettings.Value.TravelersCollectionName);
+            var mongoClient = new MongoClient(dbSettings.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(dbSettings.Value.DatabaseName);
+            _travelerCollection = mongoDatabase.GetCollection<Traveler>(dbSettings.Value.TravelersCollectionName);
+            _logger = logger; // Initialize the logger
         }
 
-        //public async Task<IEnumerable<Category>> GetAllAsyc()
-        //{
-        //    var categories = await _categoryCollection.Find(_ => true).ToListAsync();
-        //    return categories;
-        //}
 
-        public async Task<IEnumerable<Traveler>> GetAllAsyc()=>
+
+        public async Task<IEnumerable<Traveler>> GetAllAsyc() =>
             await _travelerCollection.Find(_ => true).ToListAsync();
 
         public async Task<Traveler> GetById(string id) =>
             await _travelerCollection.Find(a => a.Id == id).FirstOrDefaultAsync();
 
         public async Task CreateAsync(Traveler Traveler) =>
-            
+
             await _travelerCollection.InsertOneAsync(Traveler);
 
-        public async Task UpdateAsync(string id,Traveler Traveler) =>
+        public async Task UpdateAsync(string id, Traveler Traveler) =>
             await _travelerCollection
             .ReplaceOneAsync(a => a.Id == id, Traveler);
 
         public async Task DeleteAysnc(string id) =>
             await _travelerCollection.DeleteOneAsync(a => a.Id == id);
+
+
+
+        public async Task<Traveler> GetByNIC(string nic)
+        {
+
+            var filter = Builders<Traveler>.Filter.Eq(x => x.NIC, nic);
+            var traveler = await _travelerCollection.Find(filter).FirstOrDefaultAsync();
+
+
+            return traveler;
+        }
+
+        public async Task<Traveler> GetByNICS(string nic)
+        {
+            var filter = Builders<Traveler>.Filter.Eq(x => x.NIC, nic);
+            var traveler = await _travelerCollection.Find(filter).FirstOrDefaultAsync();
+            return traveler;
+        }
+        public async Task DeleteByNIC(string nic)
+        {
+            var filter = Builders<Traveler>.Filter.Eq(x => x.NIC, nic);
+            await _travelerCollection.DeleteOneAsync(filter);
+        }
+
+
+
     }
 }
