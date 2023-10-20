f6176e48fe0d3a5633ddeb5c1f67002080c57c02
IT20131456 <it20131456@my.sliit.lk>
2023-10-20 10:14:07 +0530

    Update Files

--- Models/DatabaseSettings.cs
+++ Models/DatabaseSettings.cs
@@ -1,4 +1,11 @@
-﻿namespace MongoDotnetDemo.Models
+﻿/**
+ * Filename: DatabaseSettings.cs
+ * Namespace: MongoDotnetDemo.Models
+ * Author: IT20131456,IT20128036,IT20127046,IT20125202
+ * Description: This class represents the database settings for the MongoDB database used in the application.
+ *              It defines various collection names and the connection details.
+ */
+namespace MongoDotnetDemo.Models
 {
     public class DatabaseSettings
     {
